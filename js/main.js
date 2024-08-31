516d8ee3579d57d59cf37284119f4c0621491b2b
inkopro <55760539+inkopro@users.noreply.github.com>
2024-09-01 02:47:16 +0500

    final_6

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const menu = document.querySelector('.header__menu')
 const menuBtn = document.querySelector('.header__top__burger');
+const body = document.body;
 
 if (menu && menuBtn) {
   menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active')
     menuBtn.classList.toggle('active')
     menuBtn.classList.toggle('rotate')
+    body.classList.toggle('lock')
   })
 }
